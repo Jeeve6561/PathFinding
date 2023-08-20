6ecbefde0e3d6424987e9f81ae2c2f060a87b24c
Jeeve6561 <smahadeo@mit.edu>
2023-08-20 00:10:30 -0400

    Finished grid drawing tool

---
+++ script.js
@@ -0,0 +1,122 @@
+GRAPH = document.getElementById("graph");
+ROWS = 40;
+COLS = 60;
+HEIGHT = GRAPH.clientHeight / ROWS;
+WIDTH = GRAPH.clientWidth / COLS;
+SETTING = null;
+START_SQUARE = null;
+END_SQUARE = null;
+START_BUTTON = document.getElementById("start");
+END_BUTTON = document.getElementById("end");
+WALL_BUTTON = document.getElementById("wall");
+DONE_BUTTON = document.getElementById("done");
+
+function MakeGrid(rows, cols) {
+  for (let r = 0; r < rows; r++) {
+    gridRow = document.createElement("div");
+    for (let c = 0; c < cols; c++) {
+      cell = document.createElement("div");
+      cell.style.height = HEIGHT + "px";
+      cell.style.width = WIDTH + "px";
+      cell.setAttribute("id", r + "-" + c);
+      cell.classList.add("square");
+      cell.onmousedown = (event) => {
+        SetCellClick(event.target);
+      };
+      cell.onmouseover = (event) => {
+        SetCellDrag(event);
+      };
+      gridRow.appendChild(cell);
+    }
+    gridRow.classList.add("row");
+    GRAPH.appendChild(gridRow);
+  }
+}
+
+function ResetGrid() {
+  DoneWithSettings();
+  GRAPH.innerHTML = "";
+  MakeGrid(ROWS, COLS);
+}
+
+function ChangeSetting(type) {
+  if (SETTING === "start") {
+    START_BUTTON.classList.remove("startpressed");
+  } else if (SETTING === "end") {
+    END_BUTTON.classList.remove("endpressed");
+  } else if (SETTING === "wall") {
+    WALL_BUTTON.classList.remove("wallpressed");
+  }
+
+  if (type === "start") {
+    START_BUTTON.classList.add("startpressed");
+  } else if (type === "end") {
+    END_BUTTON.classList.add("endpressed");
+  } else if (type === "wall") {
+    WALL_BUTTON.classList.add("wallpressed");
+  }
+
+  SETTING = type;
+}
+
+function DoneWithSettings() {
+  if (SETTING === "start") {
+    START_BUTTON.classList.remove("startpressed");
+  } else if (SETTING === "end") {
+    END_BUTTON.classList.remove("endpressed");
+  } else if (SETTING === "wall") {
+    WALL_BUTTON.classList.remove("wallpressed");
+  }
+  SETTING = null;
+}
+
+function SetCellClick(cell) {
+  if (SETTING === "start") {
+    if (START_SQUARE != null) {
+      oldStart = document.getElementById(START_SQUARE);
+      oldStart.classList = [];
+      oldStart.classList.add("square");
+    }
+    START_SQUARE = cell.id;
+    cell.classList = [];
+    cell.classList.add("startsquare");
+  } else if (SETTING === "end") {
+    if (END_SQUARE != null) {
+      oldEnd = document.getElementById(END_SQUARE);
+      oldEnd.classList = [];
+      oldEnd.classList.add("square");
+    }
+    END_SQUARE = cell.id;
+    cell.classList = [];
+    cell.classList.add("endsquare");
+  } else if (
+    SETTING === "wall" &&
+    cell.id != START_SQUARE &&
+    cell.id != END_SQUARE
+  ) {
+    cell.classList = [];
+    cell.classList.add("wallsquare");
+  }
+}
+
+function SetCellDrag(event) {
+  if (
+    SETTING === "wall" &&
+    event.buttons === 1 &&
+    event.target.id != START_SQUARE &&
+    event.target.id != END_SQUARE
+  ) {
+    cell = event.target;
+    cell.classList.remove("square");
+    cell.classList.add("wallsquare");
+    console.log(event.buttons);
+  }
+}
+
+window.onresize = () => {
+  ResetGrid();
+};
+window.onmousedown = (event) => {
+  event.preventDefault();
+};
+MakeGrid(ROWS, COLS);
